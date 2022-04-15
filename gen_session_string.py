bf8280cbf4f804946b83bbaced8da11394bd88b7
jeelpatel231 <jeelpatel231@gmail.com>
2022-04-15 12:54:52 +0530

    add session_string gen script

---
+++ gen_session_string.py
@@ -0,0 +1,24 @@
+from pyrogram import Client
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+try:
+    api_id = config["API_ID"]
+except KeyError:
+    api_id = int(input("Enter API ID : "))
+
+try:
+    api_hash = config["API_HASH"]
+except KeyError:
+    api_hash = str(input("Enter API HASH : "))
+
+app = Client(":memory:",api_id=api_id,api_hash=api_hash)
+
+with app:
+    sess_str = app.export_session_string()
+    app.send_message("me",sess_str)
+    print("--------------")
+    print(sess_str)
+    print("--------------")
+    print("this is also sent to your saved messages")
