20d23a28035145e43cb5c6b22ba17837a6260dfc
jeelpatel231 <jeelpatel231@gmail.com>
2022-04-25 14:10:17 +0530

    update template for new code, (spawn a thread)

--- modules/template_module.py
+++ modules/template_module.py
@@ -1,2 +1,2 @@
-async def template_module(client,message): # fixed
-    await message.reply_text("template_module successfully executed", quote=True) # your code
+def template_module(client,message): # fixed
+    message.reply_text("template_module successfully executed", quote=True) # your code
