6767d3d88010f3a26c859b833e1c0ae0e7d88fbf
jeelpatel231 <jeelpatel231@gmail.com>
2022-04-26 17:23:20 +0530

    ignore files starting with__

--- modules/__init__.py
+++ modules/__init__.py
@@ -6,7 +6,10 @@ import glob
 modules = glob.glob(join(dirname(__file__), "*.py"))
 
 # import EVERYTHING in modules folder
-__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
+# __xxxxxx , ignored
+# _xxxx, runnable scripts
+# xxx, modules you can use in chat
+__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not basename(f).startswith('__')]
 
 # add "_" at the start of any module to ignore loading it as module
 # it will execute seperately as a runnable
