a49e1b18d8d03c97cbd499133d076635809dbd69
jeelpatel231 <jeelpatel231@gmail.com>
2022-04-26 15:27:17 +0530

    import normal scripts too and differentiate using _

--- modules/__init__.py
+++ modules/__init__.py
@@ -5,5 +5,9 @@ from os.path import dirname, basename, isfile, join
 import glob
 modules = glob.glob(join(dirname(__file__), "*.py"))
 
-# add "__" at the start of any module to ignore loading it.
-__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.startswith('__')]
+# import EVERYTHING in modules folder
+__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
+
+# add "_" at the start of any module to ignore loading it as module
+# it will execute seperately as a runnable
+loadable_mods = [ mods for mods in __all__ if not basename(mods).startswith('_') ]
