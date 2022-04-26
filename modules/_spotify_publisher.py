f318a2cd7e94d52542edc269d8a51dbac6527013
jeelpatel231 <jeelpatel231@gmail.com>
2022-04-27 02:48:10 +0530

    add nothing playing check

--- modules/_spotify_publisher.py
+++ modules/_spotify_publisher.py
@@ -10,6 +10,7 @@ INSERT_QUERY = "INSERT INTO spotifypublishdata (track_id,last_played) VALUES (%s
 UPDATE_QUERY = "UPDATE spotifypublishdata SET last_played = CURRENT_TIMESTAMP where track_id = %s"
 SELECT_QUERY = "select extract(epoch from last_played) from spotifypublishdata WHERE track_id=%s"
 CREATE_TABLE = "CREATE TABLE IF NOT EXISTS spotifyPublishData ( track_id VARCHAR(50) PRIMARY KEY, last_played TIMESTAMP )"
+POLL_INTERVAL = 30
 
 def runnable(client):
     try:
@@ -27,6 +28,9 @@ def runnable(client):
     while True:
         print("POLLED")
         data = get_details()
+        if data == None:
+            time.sleep(POLL_INTERVAL)
+            continue
         result = cur.execute(SELECT_QUERY,(data[6],)).fetchall()
         if result == []:
             cur.execute(INSERT_QUERY,(data[6],))
@@ -38,4 +42,4 @@ def runnable(client):
             conn.commit()
             photo = make_image(data,client.get_me().username)
             client.send_photo(chat_id,photo,caption=f"[HERE]({data[4]})")
-        time.sleep(10) # sleep 30 before polling again
+        time.sleep(POLL_INTERVAL)
