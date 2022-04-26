62a134c0ecefb23ce4bb9ca9b4b61dce94553e30
jeelpatel231 <jeelpatel231@gmail.com>
2022-04-26 15:28:00 +0530

    refractor code to allow circular imports and run only if file is executed

--- main.py
+++ main.py
@@ -18,24 +18,28 @@ app = Client(
     api_hash=os.environ["API_HASH"]
 )
 
-# this function map is the workaround/fix for eval is evil, but still uses eval to be made, pretty ironic :P
-# map/dict of all functions/modules available in ./modules folder
-function_map = {}
-for i in modules.__all__:
-    function_map[f".{str(i)}"] = eval(f"{i}.{i}")
+def main():
+    # this function map is the workaround/fix for eval is evil, but still uses eval to be made, pretty ironic :P
+    # map/dict of all functions/modules available in ./modules folder
+    function_map = {}
+    for i in modules.loadable_mods:
+        function_map[f".{str(i)}"] = eval(f"{i}.{i}")
 
-# message filter to check if the message is from user and has "." at the start
-async def message_filter(_,__,message):
-    if message.text is not None and message.from_user is not None:
-        return message.from_user.is_self and message.text.startswith('.')
+    # message filter to check if the message is from user and has "." at the start
+    async def message_filter(_,__,message):
+        if message.text is not None and message.from_user is not None:
+            return message.from_user.is_self and message.text.startswith('.')
 
 
-# on message listener and function execute ONLY if its in function_map
-@app.on_message(filters.create(message_filter))
-async def my_function(client, message):
-    method = message.text.split(" ",1)[0]
-    if method in function_map.keys():
-        Thread(target=function_map[method],args=(client,message)).start()
+    # on message listener and function execute ONLY if its in function_map
+    @app.on_message(filters.create(message_filter))
+    async def my_function(client, message):
+        method = message.text.split(" ",1)[0]
+        if method in function_map.keys():
+            Thread(target=function_map[method],args=(client,message)).start()
 
-# :P
-app.run()
+    # :P
+    app.run()
+
+if __name__ == "__name__":
+    main()
