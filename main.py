4791e940ef148b2b8d98d0a37a76dc6623cd6f87
jeelpatel231 <jeelpatel231@gmail.com>
2022-04-27 21:02:14 +0530

    add enable/disable command for mods

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ def main():
     function_map = {}
     for i in modules.loadable_mods:
         function_map[f".{str(i)}"] = eval(f"{i}.{i}")
+    backup_function_map = function_map.copy()
 
     # message filter to check if the message is from user and has `text` at the start
     def dynamic_data_filter(data):
@@ -35,12 +36,35 @@ def main():
                 return message.from_user.is_self and message.text.startswith(flt.data)
         return filters.create(func, data=data)
 
+    @app.on_message(dynamic_data_filter(".disable"))
+    async def _(_,message):
+        args = message.text.split(" ")
+        for i in args[1:]:
+            i = f".{i}"
+            if i in function_map:
+                function_map.pop(i)
+                await message.reply_text(f"`disabled module {i}`")
+                return
+            await message.reply_text(f"`module {i} not found...`")
+
+    @app.on_message(dynamic_data_filter(".enable"))
+    async def _(_,message):
+        args = message.text.split(" ")
+        for i in args[1:]:
+            i = f".{i}"
+            if i in backup_function_map:
+                function_map.update({i : backup_function_map[i]})
+                await message.reply_text(f"`loaded module {i}!`")
+                return
+            await message.reply_text(f"`module {i} not found to load...`")
+
     # on message listener and function execute ONLY if its in function_map
     @app.on_message(dynamic_data_filter("."))
-    async def my_function(client, message):
+    async def _(client, message):
         method = message.text.split(" ",1)[0]
         if method in function_map.keys():
             Thread(target=function_map[method],args=(client,message)).start()
+
 
     # start all the runnable threads
     for i in modules.runnable_scripts:
