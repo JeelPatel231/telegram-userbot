7a3248f422db8c94a183ceec176dfdc473d1fcb6
jeelpatel231 <jeelpatel231@gmail.com>
2022-04-30 01:30:37 +0530

    add speedtest module for internet

---
+++ modules/speedtest.py
@@ -0,0 +1,10 @@
+from speedtest import Speedtest
+
+def speedtest(_,mess):
+    mess.edit("`Running Speedtest...`")
+    st = Speedtest()
+    dl = st.download()/1048576
+    ul = st.upload()/1048576 if "-u" in mess.text else None
+    res = f"`Results:\nDL = {dl:.2f} mbps\n`"
+    if ul is not None: res += f"`UP = {ul:.2f} mbps`" 
+    mess.edit(res)
